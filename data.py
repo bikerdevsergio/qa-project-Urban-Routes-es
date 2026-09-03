74fc670b42e505114eaeba0c495092bf66a3da4b
bikerdevsergio <bikerdev_sergio@raccoondev.dev>
2026-09-03 13:11:00 -0500

    Hacer independientes las pruebas de Urban Routes
    Reemplaza el driver compartido a nivel de clase por un fixture de
    pytest que crea una sesion nueva por cada test, y agrega funciones
    auxiliares (prepare_comfort_order, prepare_phone_and_card) para que
    cada test_... arme sus propias precondiciones. Ahora cada prueba se
    puede ejecutar sola, en cualquier orden, sin depender de las demas
    (por ejemplo: pytest -k test_fill_card).

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-3985c038-0de8-4a9a-84fd-42b78454fa2b.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-91af9a00-8529-4c08-90de-67bbbe80e00e.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import data
+import pytest
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
@@ -165,64 +166,90 @@ class UrbanRoutesPage:
     def is_taxi_search_modal_visible(self):
         return self.wait.until(expected_conditions.visibility_of_element_located(self.taxi_search_modal)).is_displayed()
 
-    def wait_for_driver_info(self, timeout=60):
+    def wait_for_driver_info(self, timeout=90):
         return WebDriverWait(self.driver, timeout).until(
             expected_conditions.visibility_of_element_located(self.driver_arrival_info)).is_displayed()
 
 
+@pytest.fixture
+def driver():
+    # necesitamos un registro adicional habilitado para recuperar el código de confirmación del teléfono
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+    drv = webdriver.Chrome(options=chrome_options)
+    drv.get(data.urban_routes_url)
+    yield drv
+    drv.quit()
+
+
+def prepare_comfort_order(routes_page):
+    """Precondición común: dirección establecida y tarifa Comfort seleccionada."""
+    routes_page.set_route(data.address_from, data.address_to)
+    routes_page.click_order_taxi_button()
+    routes_page.select_comfort_tariff()
+
+
+def prepare_phone_and_card(routes_page):
+    """Precondición para las pruebas que necesitan poder enviar el pedido."""
+    prepare_comfort_order(routes_page)
+    routes_page.fill_phone_number(data.phone_number)
+    routes_page.add_credit_card(data.card_number, data.card_code)
+
+
 class TestUrbanRoutes:
 
-    driver = None
-
-    @classmethod
-    def setup_class(cls):
-        # necesitamos un registro adicional habilitado para recuperar el código de confirmación del teléfono
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
-        cls.driver = webdriver.Chrome(options=chrome_options)
-
-    def test_set_route(self):
-        self.driver.get(data.urban_routes_url)
-        routes_page = UrbanRoutesPage(self.driver)
-        TestUrbanRoutes.routes_page = routes_page
+    def test_set_route(self, driver):
+        routes_page = UrbanRoutesPage(driver)
         address_from = data.address_from
         address_to = data.address_to
         routes_page.set_route(address_from, address_to)
         assert routes_page.get_from() == address_from
         assert routes_page.get_to() == address_to
 
-    def test_select_comfort_tariff(self):
-        self.routes_page.click_order_taxi_button()
-        self.routes_page.select_comfort_tariff()
-        assert self.routes_page.is_comfort_tariff_selected()
+    def test_select_comfort_tariff(self, driver):
+        routes_page = UrbanRoutesPage(driver)
+        prepare_comfort_order(routes_page)
+        assert routes_page.is_comfort_tariff_selected()
 
-    def test_fill_phone_number(self):
-        self.routes_page.fill_phone_number(data.phone_number)
-        assert self.routes_page.get_phone_number() == data.phone_number
+    def test_fill_phone_number(self, driver):
+        routes_page = UrbanRoutesPage(driver)
+        prepare_comfort_order(routes_page)
+        routes_page.fill_phone_number(data.phone_number)
+        assert routes_page.get_phone_number() == data.phone_number
 
-    def test_fill_card(self):
-        self.routes_page.add_credit_card(data.card_number, data.card_code)
-        assert self.routes_page.get_linked_card_value() != ''
+    def test_fill_card(self, driver):
+        routes_page = UrbanRoutesPage(driver)
+        prepare_comfort_order(routes_page)
+        routes_page.fill_phone_number(data.phone_number)
+        routes_page.add_credit_card(data.card_number, data.card_code)
+        assert routes_page.get_linked_card_value() != ''
 
-    def test_comment_for_driver(self):
-        self.routes_page.write_message_for_driver(data.message_for_driver)
-        assert self.routes_page.get_message_for_driver() == data.message_for_driver
+    def test_comment_for_driver(self, driver):
+        routes_page = UrbanRoutesPage(driver)
+        prepare_comfort_order(routes_page)
+        routes_page.write_message_for_driver(data.message_for_driver)
+        assert routes_page.get_message_for_driver() == data.message_for_driver
 
-    def test_order_blanket_and_tissues(self):
-        self.routes_page.order_blanket_and_tissues()
-        assert self.routes_page.is_blanket_and_tissues_ordered()
+    def test_order_blanket_and_tissues(self, driver):
+        routes_page = UrbanRoutesPage(driver)
+        prepare_comfort_order(routes_page)
+        routes_page.order_blanket_and_tissues()
+        assert routes_page.is_blanket_and_tissues_ordered()
 
-    def test_order_two_ice_creams(self):
-        self.routes_page.order_ice_creams(2)
-        assert self.routes_page.get_ice_cream_count() == 2
+    def test_order_two_ice_creams(self, driver):
+        routes_page = UrbanRoutesPage(driver)
+        prepare_comfort_order(routes_page)
+        routes_page.order_ice_creams(2)
+        assert routes_page.get_ice_cream_count() == 2
 
-    def test_car_search_model(self):
-        self.routes_page.submit_order()
-        assert self.routes_page.is_taxi_search_modal_visible()
+    def test_car_search_model(self, driver):
+        routes_page = UrbanRoutesPage(driver)
+        prepare_phone_and_card(routes_page)
+        routes_page.submit_order()
+        assert routes_page.is_taxi_search_modal_visible()
 
-    def test_driver_info(self):
-        assert self.routes_page.wait_for_driver_info()
-
-    @classmethod
-    def teardown_class(cls):
-        cls.driver.quit()
+    def test_driver_info(self, driver):
+        routes_page = UrbanRoutesPage(driver)
+        prepare_phone_and_card(routes_page)
+        routes_page.submit_order()
+        assert routes_page.wait_for_driver_info()
